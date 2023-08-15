158e93abad81c92897337a94dc89c71b1683b9ec
FayozbekOlimov <fayozbekdeveloper@gmail.com>
2023-08-15 10:52:00 +0500

    first commit

---
+++ src/components/UserDataTable.js
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import useDebounce from "../hooks/useDebounce";
+import useGenerateData from "../hooks/useGenerateUser";
+import { exportToCSV, getRandomValue } from "../utils";
+
+const UserDataTable = () => {
+  const [region, setRegion] = useState("ru");
+  const [seed, setSeed] = useState(() => getRandomValue());
+  const [errorRate, setErrorRate] = useState(0);
+
+  const debouncedErrorRate = useDebounce(errorRate, 300);
+  const { data, isLoading } = useGenerateData(region, seed, debouncedErrorRate);
+
+  useEffect(() => {
+    setErrorRate(0);
+  }, [seed]);
+
+  return (
+    <div className="container-xl my-4">
+      <div className="row mb-3">
+        <div className="col col-12 col-md-6 col-lg-4 col-xl-2 my-2">
+          <label htmlFor="region" className="form-label fw-medium">
+            Select Region:
+          </label>
+          <select
+            id="region"
+            className="form-select"
+            value={region}
+            onChange={(e) => setRegion(e.target.value)}
+          >
+            <option value="ru">Russia</option>
+            <option value="pl">Poland</option>
+            <option value="tr">Turkey</option>
+          </select>
+        </div>
+        <div className="col col-12 col-md-6 col-lg-4 col-xl-3 my-2">
+          <label htmlFor="seed" className="form-label fw-medium">
+            Seed Value:
+          </label>
+          <div className="d-flex gap-2">
+            <button
+              className="btn btn-primary w-100"
+              onClick={() => setSeed(getRandomValue())}
+            >
+              Random Seed
+            </button>
+            <input
+              type="number"
+              className="form-control"
+              id="seed"
+              min={0}
+              value={seed}
+              onChange={(e) => setSeed(Number(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="col col-12 col-md-9 col-lg-4 col-xl-5 my-2">
+          <label htmlFor="slider" className="form-label fw-medium">
+            Error range
+          </label>
+          <div className="d-flex align-items-center gap-2">
+            <input
+              type="range"
+              className="form-range"
+              id="slider"
+              min={0}
+              max={10}
+              step={0.25}
+              value={errorRate}
+              onChange={(e) => setErrorRate(Number(e.target.value))}
+            />
+            <input
+              type="number"
+              className="form-control"
+              min={0}
+              step={0.25}
+              max={1000}
+              value={errorRate}
+              onChange={(e) => setErrorRate(Number(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="col col-12 col-md-3 col-lg-4 col-xl-2 my-2">
+          <label htmlFor="export-btn" className="form-label fw-medium">
+            Export
+          </label>
+          <button
+            id="export-btn"
+            className="btn btn-primary w-100"
+            onClick={() => exportToCSV(data)}
+          >
+            Export to CSV
+          </button>
+        </div>
+      </div>
+
+      <div className="table-wrapper">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th className="border border-primary border-2">Index</th>
+              <th className="border border-primary border-2">
+                Random Identifier
+              </th>
+              <th className="border border-primary border-2">Name</th>
+              <th className="border border-primary border-2">Address</th>
+              <th className="border border-primary border-2">Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((record, index) => (
+              <tr key={record.randomID}>
+                <td className="border border-primary border-2">{index + 1}</td>
+                <td className="border border-primary border-2">
+                  {record.randomID}
+                </td>
+                <td className="border border-primary border-2">
+                  {record.fullName}
+                </td>
+                <td className="border border-primary border-2">
+                  {record.address}
+                </td>
+                <td className="border border-primary border-2">
+                  {record.phoneNumber}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {isLoading && (
+          <div className="d-flex gap-2">
+            {Array.from(Array(3)).map((_, ind) => (
+              <div
+                key={ind}
+                className="spinner-grow spinner-grow-sm text-primary my-2"
+                role="status"
+              >
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UserDataTable;
---
+++ src/hooks/useGenerateUser.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import { generateDataWithErrors } from "../utils";
+import { faker } from "../utils/constants";
+
+const useGenerateData = (region, seed, errorRate) => {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const generateRandomData = (num, seedValue) => {
+    const newData = [];
+
+    const pageSeed = seedValue + page;
+    faker[region].seed(pageSeed);
+
+    for (let i = 0; i < num; i++) {
+      const randomID = faker[region].string.uuid();
+      const fullName = faker[region].person.fullName();
+      const address = faker[region].location.streetAddress();
+      const phoneNumber = faker[region].phone.number();
+
+      newData.push({
+        randomID,
+        fullName,
+        address,
+        phoneNumber,
+      });
+    }
+
+    setData((prevData) => [...prevData, ...newData]);
+    setIsLoading(false);
+  };
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition + windowHeight >= documentHeight - 50 && !isLoading) {
+      setIsLoading(true);
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    const newDataWithErrors = generateDataWithErrors(data, region, errorRate);
+    setData(newDataWithErrors);
+  }, [errorRate]);
+
+  useEffect(() => {
+    generateRandomData(10, seed);
+  }, [page]);
+
+  useEffect(() => {
+    setData([]);
+    generateRandomData(20, seed);
+  }, [region, seed]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return { data, isLoading };
+};
+
+export default useGenerateData;
